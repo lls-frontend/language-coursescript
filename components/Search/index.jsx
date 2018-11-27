264bbc1b87376e07cc0608e451739d287ed62f6b
bill.zhu <bill.zhu@liulishuo.com>
2018-11-27 17:46:48 +0800

    feat: resource tag -> source

--- components/Search/index.jsx
+++ components/Search/index.jsx
@@ -35,6 +35,7 @@ export default class Search extends Component {
 
   state = {
     filter: {},
+    sources: [],
     result: { list: [], count: 0 },
     error: '',
     page: 1,
@@ -87,7 +88,7 @@ export default class Search extends Component {
     )
   }
 
-  fetch = async (page, { resourceType, searchType, tag, search }) => {
+  fetch = async (page, { resourceType, searchType, source, search }) => {
     this.setState({ isFetching: true, showResults: true, error: '' })
     try {
       if (resourceType === 'Pictures' && searchType === 'ID') {
@@ -101,13 +102,10 @@ export default class Search extends Component {
         return
       }
 
-      const baseQuery = { page, page_size: PAGE_SIZE, query: search }
-
       if (searchType === 'Filename') {
         const res = await request.get(
           `/${resourceType.toLowerCase()}?${stringify({
-            ...baseQuery,
-            tag: tag.toLowerCase()
+            page, page_size: PAGE_SIZE, query: search, source
           })}`
         )
         this.setState({
@@ -151,6 +149,21 @@ export default class Search extends Component {
     }
   }
 
+  fetchSources = async () => {
+    try {
+      const sources = await request.get('/sources')
+      this.setState({
+        sources: [
+          'All',
+          ...sources.filter(item => item !== 'All' && item !== 'None'),
+          'None',
+        ]
+      })
+    } catch (error) {
+      this.setState({ error: error.message })
+    }
+  }
+
   handleSearch = async (filter) => {
     this.fetch(1, filter)
     this.setState({ filter, page: 1 })
@@ -165,13 +178,17 @@ export default class Search extends Component {
 
   handleClose = () => this.props.onClose()
 
+  componentDidMount() {
+    this.fetchSources()
+  }
+
   render() {
     const { focus } = this.props
-    const { page, result } = this.state
+    const { sources, page, result } = this.state
 
     return (
       <div className="search">
-        <Filter focus={focus} onSearch={this.handleSearch} />
+        <Filter sources={sources} focus={focus} onSearch={this.handleSearch} />
         {this.renderResults()}
         {result.count > PAGE_SIZE && (
           <Pagination
