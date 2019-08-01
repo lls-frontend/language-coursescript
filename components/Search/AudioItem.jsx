ddac3b4fcde659a8b12d58cac27f4bff83d97739
Yinjia <0807yinjia@gmail.com>
2019-08-01 10:12:51 +0800

    feat: (#132)

--- components/Search/AudioItem.jsx
+++ components/Search/AudioItem.jsx
@@ -1,30 +1,35 @@
-'use babel'
+'use babel';
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import dayjs from 'dayjs'
+import React from 'react';
+import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 
 const AudioItem = ({ data, onCopy }) => {
-  const handleCopy = (e) => {
-    const type = e.target.innerHTML.toLowerCase().replace(/\s\w/, s => s.trim().toUpperCase())
+  const handleCopy = e => {
+    const type = e.target.innerHTML
+      .toLowerCase()
+      .replace(/\s\w/, s => s.trim().toUpperCase());
     const contents = {
       id: data.id,
       name: data.description,
       code: `Audio(id=${data.id}):${data.description}`,
       trCode: `TR(id=${data.id}):${data.description}`
-    }
+    };
 
-    onCopy(contents[type] || '')
-  }
+    onCopy(contents[type] || '');
+  };
 
   return (
-    <div className="search-item audio">
-      <p>{data.createdAt && dayjs(data.createdAt).format('YYYY-MM-DD HH:mm')}</p>
+    <div className='search-item audio'>
+      <p>
+        {data.createdAt && dayjs(data.createdAt).format('YYYY-MM-DD HH:mm')}
+      </p>
       <p>{data.id}</p>
       <p>{data.description}</p>
-      <div className="search-item-audio">
-        <audio src={data.url} controls></audio>
-        <div className="search-item-buttons">
+      <p>{data.source}</p>
+      <div className='search-item-audio'>
+        <audio src={data.url} controls />
+        <div className='search-item-buttons'>
           <p>Copy:</p>
           <span onClick={handleCopy}>TR Code</span>
           <span onClick={handleCopy}>Code</span>
@@ -33,17 +38,17 @@ const AudioItem = ({ data, onCopy }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 AudioItem.propTypes = {
   data: PropTypes.shape({
     url: PropTypes.string,
     id: PropTypes.string.isRequired,
     description: PropTypes.string,
-    filename: PropTypes.string,
+    filename: PropTypes.string
   }).isRequired,
-  onCopy: PropTypes.func.isRequired,
-}
+  onCopy: PropTypes.func.isRequired
+};
 
-export default AudioItem
+export default AudioItem;
